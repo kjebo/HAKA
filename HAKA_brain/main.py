b98501411328fea0b04a78f0b4e169cbae3d8ff5
kjebo <kjellbovin@hotmail.com>
2022-10-19 14:58:25 +0200

    changes in who_is API endpoint

--- HAKA_brain/main.py
+++ HAKA_brain/main.py
@@ -1,20 +1,22 @@
-# importing Flask and other modules
 from flask import Flask, request, render_template
- 
-# Flask constructor
+from curses import keyname
+import requests
+from pprint import pprint
+
+BASE = "http://127.0.0.1:5000/" 
+
 app = Flask(__name__)  
- 
-# A decorator used to tell the application
-# which URL is associated function
 @app.route('/', methods =["GET", "POST"])
 def gfg():
     if request.method == "POST":
-       # getting input with name = fname in HTML form
        ip = request.form.get("ip")
        # getting input with name = lname in HTML form
        domain = request.form.get("domain")
-       return "Result: "+ ip + domain
+       response = requests.get(BASE + ip)
+       return response.json()
     return render_template("form.html")
+
  
 if __name__=='__main__':
    app.run(host='localhost', port=5500, debug=True)
+
--- micros_check_ip/who_is_ip.py
+++ micros_check_ip/who_is_ip.py
@@ -1,44 +1,27 @@
 from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
 from ipwhois import IPWhois
 import nmap
 import re
 
-nm = nmap.PortScanner()
-
-
-#ip = "62.63.215.64"
-ip = "193.15.26.122"
-
-# Using Whois to check the IP-address
-obj = IPWhois(ip)
-results = obj.lookup_rdap(depth=1)
-
-# Grepping info about network handle from the whois result
-network_handle = results['network']['handle']
-network_handle_list = re.findall(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})',network_handle)
-last_ip_dress = re.findall('\d{1,3}$',network_handle_list[1])
-scan_this = f'{network_handle_list[0]}-{last_ip_dress[0]}'
-
-# Doing a ping scan on the on All IPs
-nm.scan(hosts=scan_this, arguments='-sP')
-
 app = Flask(__name__)
+  
+@app.route('/<string:ip>')
+def get_ip(ip):
+    nm = nmap.PortScanner()
+    obj = IPWhois(ip)
+    results = obj.lookup_rdap(depth=1)
+    network_handle = results['network']['handle']
+    network_handle_list = re.findall(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})',network_handle)
+    last_ip_dress = re.findall('\d{1,3}$',network_handle_list[1])
+    scan_this = f'{network_handle_list[0]}-{last_ip_dress[0]}'
+    result = nm.scan(hosts=scan_this, arguments='-sP')
+    
+    print(network_handle_list)
+    print(network_handle)
 
-ip_addr = [
-    { 'IP-address': nm.all_hosts()}
-]
+    return results
+ 
 
-''''''
-@app.route('/')
-def get_ip():
-    return jsonify(ip_addr)
-
-
-@app.route('/', methods=['POST'])
-def check_ip():
-    ip_addr.append(request.get_json())
-    return '', 204
-
-
-if __name__ =="__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
